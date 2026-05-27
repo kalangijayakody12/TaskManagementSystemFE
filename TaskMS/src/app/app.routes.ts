77a154b947197d8d6bbd2b78539da7c4864a47ce
kalangijayakody12 <kalangi.j@digitustec.com.au>
2026-05-27 10:13:21 +0530

    feat: Login page implementation

--- TaskMS/src/app/app.routes.ts
+++ TaskMS/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Login } from './core/features/auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Login},
+];
---
+++ TaskMS/src/app/core/features/auth/login/login.ts
@@ -0,0 +1,46 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../services/auth-service';
+import { LoginDto } from '../dto/login.dto';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule, CommonModule, RouterLink],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+  
+  loginForm:FormGroup;
+
+  constructor(private fb:FormBuilder, private authService:AuthService){
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+      const loginDto:LoginDto = {
+        email:this.loginForm.value.email,
+        password:this.loginForm.value.password
+      }
+      this.authService.login(loginDto).subscribe({
+          next: () => {
+            console.log('Login successful');
+          },
+          error: (err) => {
+            console.error('Login failed', err);
+          }
+      })
+    }
+  }
+
+
+
+
+}
