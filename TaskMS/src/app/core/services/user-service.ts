f4c4858c0528c9472c3022cacb038fc3fcaa5790
kalangijayakody12 <kalangi.j@digitustec.com.au>
2026-06-03 11:59:06 +0530

    feat: user service implementation

---
+++ TaskMS/src/app/core/services/user-service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private apiUrl = `http://localhost:3000/user`;
+
+  constructor(private http: HttpClient) {}
+
+  getAllUsers(): Observable<any> {
+    return this.http.get(`${this.apiUrl}`);
+  }
+
+}
